6ff0255b5879e847d9aa928b5ddc7b88b7862b88
magluf <sergio.magluf@accenture.com>
2019-09-05 11:45:08 -0300

    Perfection on description.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),
   cssvars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
   cssImport = require('postcss-import'),
-  plumber = require('gulp-plumber');
+  plumber = require('gulp-plumber'),
+  mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
   return gulp
     .src('./app/assets/styles/styles.css')
     .pipe(plumber())
-    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
